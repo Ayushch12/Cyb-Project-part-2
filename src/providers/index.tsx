dc4fec460f4c309bb17b77e6af66d073b19c8dd9
Ayush chalisee <ayushchalise12@gmail.com>
2024-07-20 00:51:01 +0200

    Removed the ReactQueryDevtools

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { useRouter } from '@/routes/hooks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Suspense } from 'react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
@@ -41,7 +40,6 @@ export default function AppProvider({
         <BrowserRouter>
           <ErrorBoundary FallbackComponent={ErrorFallback}>
             <QueryClientProvider client={queryClient}>
-              <ReactQueryDevtools />
               <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                 {children}
               </ThemeProvider>
